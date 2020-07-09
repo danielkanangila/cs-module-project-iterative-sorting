662397c7c3678e55cec4e7c3e2432254e78fb6b3
danielkanangila <daniel.kanangila@gmail.com>
2020-07-08 22:28:23 -0400

    pass binary_search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -9,7 +9,20 @@ def linear_search(arr, target):
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    start = 0
+    end = (len(arr) - 1)
 
-    # Your code here
+    found = False
 
+    while end >= start and not found:
+        middle_index = (start + end) // 2
+
+        if arr[middle_index] == target:
+            found = True
+            return arr.index(target)
+        else:
+            if target < arr[middle_index]:
+                end = middle_index - 1
+            if target > arr[middle_index]:
+                start = middle_index + 1
     return -1  # not found
